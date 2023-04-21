ba2deba20411a342df03b60d7dfa8f04c5a5746e
mofse <hahya001@osloskolen.no>
2023-04-21 13:31:26 +0200

    features

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function lagreVerdi(id){
-    var verdi = document.getElementById(id).value
-    var elementthing = document.getElementById("selectCont").value
-    console.log("verdi lagret som " + verdi)
-    console.log("verdi lagret som " + elementthing)
-    document.getElementById(elementthing).style.backgroundColor = verdi
+
+var boxses = ["box1", "box2", "box3"];
+var verdi = Number();
+var elementthing = Number();
+
+
+function lagreVerdi(){
+    var verdi = document.getElementById("colin").value;                    //Color input
+    var elementthing = document.getElementById("selectCont").value;  //Box input
+    var rangeval = document.getElementById("test").value;           //Range input
+
+    console.log("color lagret som " + verdi);
+    console.log("box lagret som " + elementthing);
+    console.log("range lagret som " + rangeval);
+
+
+    document.getElementById("testus").innerHTML = rangeval;
+    document.getElementById(elementthing).style.backgroundColor = verdi;
+}
+
+function updateBoks(){
+    document.getElementById("elementsCont").innerHTML += '<div class="elements" id="box' + (boxses.length + 1) + '"></div>';
+    document.getElementById("selectCont").innerHTML += '<option value="box' + (boxses.length + 1) + '">boks ' + (boxses.length + 1) + '</option>'
+    boxses.push('box' + (boxses.length + 1));
+    console.log(boxses)
 }
